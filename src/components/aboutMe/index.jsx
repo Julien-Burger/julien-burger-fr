0ccfae10fefe616d8541f89bcf4408d4e5244725
Julien Burger <julienburger1998@gmail.com>
2024-02-19 20:50:48 +0100

    Adding "about me" section

--- src/components/aboutMe/index.jsx
+++ src/components/aboutMe/index.jsx
@@ -2,14 +2,19 @@ import "./style.scss";
 
 function AboutMe() {
     return (
-        <div className="aboutMe" id="aboutMe">
-            <div className="title">
-                <div></div>
-                <div></div>
-                <div></div>
+        <section className="aboutMe" id="aboutMe">
+            <div>
                 <h2>À propos de moi</h2>
+                <p>
+                    Passionné par le développement de <b>jeux vidéo et le développement web</b> depuis toujours, j'ai débuté la
+                    programmation à l'âge de 15 ans en autodidacte.
+                </p>
+                <p>
+                    Spécialisé dans le développement <b>full-stack</b>, j'ai eu l'occasion de développer divers applications web
+                    et mobile ainsi qu'un <b>logiciel professionnel</b> d'impression.
+                </p>
             </div>
-        </div>
+        </section>
     );
 }
 
