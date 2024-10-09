4ea6f9dfa78f52fdee558b4aa7a0e6e3a2d16202
Julien-Burger <julienburger1998@gmail.com>
2024-10-09 12:35:30 +0200

    adding supported languages (fr, en)

--- src/app/i18n.js
+++ src/app/i18n.js
@@ -3,9 +3,13 @@ import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 import Backend from "i18next-http-backend";
 
-i18next.use(Backend).use(LanguageDetector).use(initReactI18next).init({
-    // debug: true,
-    fallbackLng: "fr",
-});
+i18next
+    .use(Backend)
+    .use(LanguageDetector)
+    .use(initReactI18next)
+    .init({
+        fallbackLng: "fr",
+        supportedLngs: ["fr", "en"],
+    });
 
 export default i18next;
